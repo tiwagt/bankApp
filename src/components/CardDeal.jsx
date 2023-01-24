a53692511f52a9a5b526be8ef4434694829938b9
Tiwa Grace <gtiwa712@gmail.com>
2023-01-24 16:47:43 +0100

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => {
+  return (
+    <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Finding a better card deal is easy.
+      With our app, you can find the best credit cards for your needs by exploring our database of offers in your area.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+  )
+}
+
+export default CardDeal
